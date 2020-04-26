a4d99e07fdaebf50839225e2f91075ad3a2c65f6
Sakshat Bhattarai <bhattaraisakshat07@gmail.com>
2020-04-26 01:53:40 -0400

    error handling

--- src/app/survey.service.ts
+++ src/app/survey.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Surveydata } from './surveydata';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 
 const SURVEY_URL = environment.sUrl;
@@ -17,16 +18,40 @@ export class SurveyService {
    }
 
    public getSurvey(id: number): Observable<Surveydata> {
-     return this.httpClient.get<Surveydata>(SURVEY_URL+ `/${id}`);
+     return this.httpClient.get<Surveydata>(SURVEY_URL+ `/${id}`)
+     .pipe(
+       retry(3),
+       catchError(this.handleError)
+     );
    }
 
-   public addSurvey(surveydata: Surveydata) {
+   public addSurvey(surveydata: Surveydata): Observable<Surveydata> {
      let options = {headers: new HttpHeaders({'Content-Type': 'application/json'})};
      let jsonData = JSON.stringify(surveydata);
-     return this.httpClient.post<Surveydata>(SURVEY_URL + '/addSurvey', jsonData, options);
+     return this.httpClient.post<Surveydata>(SURVEY_URL + '/addSurvey', jsonData, options)
+     .pipe(
+      catchError(this.handleError)
+      );
    }
 
    public getAllSurveys():Observable<any> {
-     return this.httpClient.get<any>(SURVEY_URL);
+     return this.httpClient.get<any>(SURVEY_URL)
+     .pipe(
+      retry(3),
+      catchError(this.handleError)
+     );
+   }
+
+   private handleError(error: HttpErrorResponse) {
+     if (error.error instanceof ErrorEvent) {
+       console.error('An error occured:', error.error.message);
+     } else {
+       console.error(
+         'Backend returned code ${error.status}, ' +
+         'body was: ${error.error}');
+     }
+     return throwError(
+       'The request cannot be fulfilled. Please try again later.'
+     )
    }
 }
