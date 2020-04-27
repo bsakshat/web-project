a5cb44302111fae495433fa52048697c0d5ad752
Sakshat Bhattarai <bhattaraisakshat07@gmail.com>
2020-04-27 01:37:25 -0400

    add survey added

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Surveydata } from '../surveydata';
-import { SurveyService } from '../survey.service'
+import { SurveyService } from '../survey.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -11,17 +12,52 @@ import { SurveyService } from '../survey.service'
 
 export class StudentComponent implements OnInit {
 
-  recoms = ['Very Likely', 'Likely', 'Unlikely']
+  like = [];
+  submitted = false;
+  data: Surveydata = new Surveydata();
 
-  data: Surveydata;
-
-  constructor(private service: SurveyService) { }
+  constructor(private service: SurveyService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  onSubmit() {
-    this.service.addSurvey(this.data);
+  onLikes(event, check) {
+    if (event.target.checked) {
+      this.like.push(check);
+    }
+    if (!event.target.checked) {
+      let index = this.like.indexOf(check);
+      if (index > -1) {
+        this.like.splice(index, 1);
+      }
+    }
   }
+
+  assignData() {
+    this.data.likes = this.data.likes ? this.data.likes : "";
+    this.data.interest = this.data.interest ? this.data.interest : "";
+    this.data.recom = this.data.recom ? this.data.recom : "";
+  }
+
+  validate() {
+    if (!this.data.first || !this.data.last || !this.data.address || !this.data.city || !this.data.state || !this.data.zipcode || !this.data.phone || !this.data.email || !this.data.tdate) {
+      console.log('Input the required values.');
+      return false;
+    }
+    return true;
+  }
+
+  onSubmit() {
+    this.assignData();
+    if (!this.validate()) {
+      return;
+    }
+    this.data.likes = this.like.join();
+    this.service.addSurvey(this.data).subscribe(()=>{
+      console.log("Survey record added");
+    });
+    this.submitted = true;
+  }
+
 
 }
